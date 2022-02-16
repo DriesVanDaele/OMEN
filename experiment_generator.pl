1f5062c36175959ce10e971855c0988d34962b44
u0077928 <dries.vandaele@cs.kuleuven.be>
2022-02-17 00:38:32 +0100

    changed quality threshold

--- experiment_generator.pl
+++ experiment_generator.pl
@@ -1,7 +1,7 @@
 data('example_data/tokheim_pancancer_somatic_CADD.pl', 'example_data/tokheim_pancancer_somatic_coverage_ranks.pl').
 network_file('example_data/network.pl').
 threshold(10**(-6)).
-pattern_quality_metric_threshold(0.5).
+pattern_quality_metric_threshold(0.67).
 mcda(weighted_sum, [0.8, 0]).
 path_definition_file('example_data/path_definition.dslp').
    
